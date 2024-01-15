a74961a224dc9f08056e9c29cadcfac1bd53b4cc
Trac Van <truongtracvan@gmail.com>
2024-01-15 15:04:04 +0700

    Update code

--- LeapYearCaculator/src/LeapYearCaculator.java
+++ LeapYearCaculator/src/LeapYearCaculator.java
@@ -1,22 +1,32 @@
+import com.sun.xml.internal.ws.policy.EffectiveAlternativeSelector;
+
 import java.util.Scanner;
+
 public class LeapYearCaculator {
     public static void main(String[] args) {
         System.out.println("Which year do you want to caculate:");
         Scanner scanner = new Scanner(System.in);
         int year = scanner.nextInt();
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0) {
-                    System.out.printf("%d is a leap year", year);
-                }else{
-                    System.out.printf("%d is NOT a leap year", year);
-                }
+        boolean isLeapYear = false;
 
-            }else {
-                System.out.printf("%d is a leap year", year);
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if (isDivisibleBy4) {
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if (isDivisibleBy100) {
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if (isDivisibleBy400) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
             }
-        }else {
-            System.out.printf("%d is NOT a leap year", year);
+        }
+        if (isLeapYear) {
+            System.out.printf("%d is leap year", year);
+        } else {
+            System.out.printf("%d is NOT leap year", year);
         }
     }
 }
+
+
