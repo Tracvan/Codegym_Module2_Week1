8aa6d799ae99c068566b08932ffea9ffe6773e86
Trac Van <truongtracvan@gmail.com>
2024-01-15 17:35:46 +0700

    Update code

--- PrimeNumberCheck/src/PrimeNumberCheck.java
+++ PrimeNumberCheck/src/PrimeNumberCheck.java
@@ -7,16 +7,19 @@ public class PrimeNumberCheck {
         System.out.print("Input number: ");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        boolean isPrime = false;
+        boolean isPrime = true;
         if(number == 2 ){
             isPrime = true;
         }else if (number > 2){
             for ( int i = 2; i<number/2 ;i++){
                 if(number % i == 0){
+                    isPrime = false;
                     break;
                 }
             }
-
+        }
+        else{
+            isPrime = false;
         }if(isPrime == true){
             System.out.print(number +" is prime");
         }else{
