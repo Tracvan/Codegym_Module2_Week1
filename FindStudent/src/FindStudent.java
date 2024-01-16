1621fc1d2e45adb97fd12e922daeb65b6bcd65cf
Trac Van <truongtracvan@gmail.com>
2024-01-16 16:22:57 +0700

    Convert Temperature

--- FindStudent/src/FindStudent.java
+++ FindStudent/src/FindStudent.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class FindStudent {
+import java.util.Scanner;
+
+public class FindStudent {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        System.out.println("Enter Student's name");
+        String name = scanner.nextLine();
+        boolean isfound = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(name)) {
+                System.out.println("Position of the students in the list " + name + " is: " + i);
+                isfound = true;
+                break;
+            }
+
+        }
+        if (!isfound) {
+            System.out.println("NOT found " + name + " in students list");
+        }
+    }
 }
+
