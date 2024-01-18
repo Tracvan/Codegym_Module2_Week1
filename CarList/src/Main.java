85fbe1559c79a7e971ad7d1684a62e47598f0da9
Trac Van <truongtracvan@gmail.com>
2024-01-18 16:05:20 +0700

    Car list

---
+++ CarList/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+     Car car1 = new Car("Mazda3","Skyactiv 3");
+        System.out.println(Car.numberOfcars);
+     Car car2 = new Car("Mazda6","Skyactiv 6");
+        System.out.println(Car.numberOfcars);
+    }
+
+}
